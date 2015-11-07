c7091843ea3829252509d790dbab13f61e0606d5
lisandro_bitencourt <lisandro_bitencourt@10.1.48.13>
2015-11-07 14:06:28 -0200

    Teste

---
+++ projetoFinal/src/main/java/com/senai/projetofinal/dao/UsuarioDAO.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.senai.projetofinal.dao;
+
+import com.senai.projetofinal.model.Usuario;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author lisandro_bitencourt
+ */
+@Stateless
+public class UsuarioDAO {
+    @PersistenceContext(unitName = "projetofinalPU")
+    private EntityManager em;
+
+    public void insere(Usuario usuario) {
+        em.persist(usuario);
+    }
+    
+}
+    
+    
+    
+    
+    
+
