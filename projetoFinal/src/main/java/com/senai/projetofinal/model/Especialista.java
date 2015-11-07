daa27b8f3a43b0b2ad637bf6094e16dc360fb1d1
lisandro_bitencourt <lisandro_bitencourt@10.1.48.13>
2015-11-07 10:54:51 -0200

    Teste

---
+++ projetoFinal/src/main/java/com/senai/projetofinal/model/Especialista.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.senai.projetofinal.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author lisandro_bitencourt
+ */
+public class Especialista extends Pessoa {
+    private String celular;
+    private String telefone;
+    private Date dtCadastro;
+    private Categoria categoria;
+    private Endereco endereco;
+
+    public Especialista(String celular, String telefone, Date dtCadastro, Integer id, String nome, String email) {
+        super(id, nome, email);
+        this.celular = celular;
+        this.telefone = telefone;
+        this.dtCadastro = dtCadastro;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Date getDtCadastro() {
+        return dtCadastro;
+    }
+
+    public void setDtCadastro(Date dtCadastro) {
+        this.dtCadastro = dtCadastro;
+    }
+    
+    
+}
