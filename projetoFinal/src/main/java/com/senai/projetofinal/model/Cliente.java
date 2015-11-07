953a4a96b2027873d617d7d928cd7c424c9f8804
lisandro_bitencourt <lisandro_bitencourt@10.1.48.13>
2015-11-07 10:27:55 -0200

    Teste

--- projetoFinal/src/main/java/com/senai/projetofinal/model/Cliente.java
+++ projetoFinal/src/main/java/com/senai/projetofinal/model/Cliente.java
@@ -1,14 +1,68 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.senai.projetofinal.model;
+
+import java.util.Date;
+
+
 
 /**
  *
  * @author lisandro_bitencourt
  */
-public class Cliente {
-    
+public class Cliente extends Pessoa {
+   private String sexo;
+   private String cpf;
+   private String celular;
+   private String telefone;
+   private Date dtNascimento;
+   private Date dtCadastro;
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Date getDtNascimento() {
+        return dtNascimento;
+    }
+
+    public void setDtNascimento(Date dtNascimento) {
+        this.dtNascimento = dtNascimento;
+    }
+
+    public Date getDtCadastro() {
+        return dtCadastro;
+    }
+
+    public void setDtCadastro(Date dtCadastro) {
+        this.dtCadastro = dtCadastro;
+    }
+   
+  
 }
