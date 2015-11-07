4952a1506cbbe57dc7c10fffa73b209c3d83792c
dudasc <dudatimbe@gmail.com>
2015-11-07 11:36:16 -0200

    Merge origin/master
    Conflicts:
    	projetoFinal/src/main/java/com/senai/projetofinal/model/Especialista.java

--- projetoFinal/src/main/java/com/senai/projetofinal/model/Agenda.java
+++ projetoFinal/src/main/java/com/senai/projetofinal/model/Agenda.java
@@ -5,10 +5,99 @@
  */
 package com.senai.projetofinal.model;
 
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 /**
  *
  * @author duda
  */
 public class Agenda {
+    private Long id;
+    private Cliente cliente;
+    private Usuario usuario;
+    private Date dataCadastro;
+    private Time horario;
+    private Integer status;
+    private List<Servico> listaServicos = new ArrayList<>();
+    private Double valorTotal;
+
+    public Agenda(Long id, Cliente cliente, Usuario usuario, Date dataCadastro, Time horario, Integer status, Double valorTotal) {
+        this.id = id;
+        this.cliente = cliente;
+        this.usuario = usuario;
+        this.dataCadastro = dataCadastro;
+        this.horario = horario;
+        this.status = status;
+        this.valorTotal = valorTotal;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public Time getHorario() {
+        return horario;
+    }
+
+    public void setHorario(Time horario) {
+        this.horario = horario;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public List<Servico> getListaServicos() {
+        return listaServicos;
+    }
+
+    public void setListaServicos(List<Servico> listaServicos) {
+        this.listaServicos = listaServicos;
+    }
+
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+    
+    
     
 }
